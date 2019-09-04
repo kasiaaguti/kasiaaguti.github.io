97533d8c2b812971431d3aa690e72a04c7276bbe
KasiaW <kasia.aguti@gmail.com>
2019-09-04 19:45:08 +0100

    mapbox

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: './src/assets', to: 'assets' }
     ]),
-    // new Dotenv(),
+    new Dotenv(),
     new webpack.DefinePlugin({
       'process.env.MAPBOX': JSON.stringify(process.env.MAPBOX)
     })
